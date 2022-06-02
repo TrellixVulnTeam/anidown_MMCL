d862e93265fac54c6719d884acce72119f4f55a7
Tinolm <OMG-ichfind-keinen-namen@protonmail.com>
2022-06-02 19:02:42 +0200

    gitignore

--- testing/installer.py
+++ testing/installer.py
@@ -7,4 +7,5 @@ def download(filename, url):
         f.write(target.content)
 
 download("main-firefox.py", "https://raw.githubusercontent.com/learning-thing/anidown/master/main-firefox.py")#get the main file
-download("getinfo.py", "https://raw.githubusercontent.com/learning-thing/anidown/master")
+download("getinfo.py", "https://raw.githubusercontent.com/learning-thing/anidown/master/getinfo.py")#get a library
+download("diff_adder.py", "https://raw.githubusercontent.com/learning-thing/anidown/diff_adder.py")
