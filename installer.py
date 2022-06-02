d3b31b509fd6d9199769bb00137cbfa8c452ae4e
Tinolm <OMG-ichfind-keinen-namen@protonmail.com>
2022-06-02 19:12:40 +0200

    installer

--- installer.py
+++ installer.py
@@ -1,4 +1,4 @@
-from os import system
+import os
 import requests
 
 def download(filename, url):
@@ -7,4 +7,14 @@ def download(filename, url):
         f.write(target.content)
 
 download("main-firefox.py", "https://raw.githubusercontent.com/learning-thing/anidown/master/main-firefox.py")#get the main file
-download("getinfo.py", "https://raw.githubusercontent.com/learning-thing/anidown/master")
+download("getinfo.py", "https://raw.githubusercontent.com/learning-thing/anidown/master/getinfo.py")#get a library
+download("diff_adder.py", "https://raw.githubusercontent.com/learning-thing/anidown/diff_adder.py")
+
+
+sos = int(input("python3[1] / python[2]?: "))
+if sos == 1:
+    os.system("python3 -m pip install -U selenium")
+
+if sos == 2:
+    os.system("python -m pip install -U selenium")
+
--- testing/installer.py
+++ testing/installer.py
@@ -1,4 +1,4 @@
-from os import system
+import os
 import requests
 
 def download(filename, url):
@@ -9,3 +9,12 @@ def download(filename, url):
 download("main-firefox.py", "https://raw.githubusercontent.com/learning-thing/anidown/master/main-firefox.py")#get the main file
 download("getinfo.py", "https://raw.githubusercontent.com/learning-thing/anidown/master/getinfo.py")#get a library
 download("diff_adder.py", "https://raw.githubusercontent.com/learning-thing/anidown/diff_adder.py")
+
+
+sos = int(input("python3[1] / python[2]?: "))
+if sos == 1:
+    os.system("python3 -m pip install -U selenium")
+
+if sos == 2:
+    os.system("python -m pip install -U selenium")
+
